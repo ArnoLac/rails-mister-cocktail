9418aacc2f0edfe46f0aa14afdcf44a997f7f098
Arnaud Lacombe <arnaud.lacombe@whisp.tech>
2019-08-15 12:28:40 +0200

    controller cocktail done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+
+list_ingredients = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients = JSON.parse(list_ingredients)
+
+(1..5).each { |x| Ingredient.create(name: ingredients["drinks"][x]["strIngredient1"]) }
